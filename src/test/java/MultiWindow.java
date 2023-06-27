f5a06e25a474e8dc5cfa15b0f335174605df0afb
ekram505026 <ekramulkarim@yahoo.com>
2023-06-27 14:59:43 -0400

    modified

---
+++ src/test/java/MultiWindow.java
@@ -0,0 +1,23 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+++
+
+public class MultiWindow {
+    public static void main(String[] args) {
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://codenboxautomationlab.com/practice/");
+        System.out.println("Title of the parent page:" + driver.getTitle());
+
+        driver.findElement(By.id("openwindow")).click();
+        System.out.println();
+
+    }
+}
