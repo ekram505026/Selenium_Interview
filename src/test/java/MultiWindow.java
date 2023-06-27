c23f88ad71ec2a60ed0d340a69b18e6272407a15
ekram505026 <ekramulkarim@yahoo.com>
2023-06-27 15:43:00 -0400

    modified

--- src/test/java/MultiWindow.java
+++ src/test/java/MultiWindow.java
@@ -2,7 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-++
+
 
 public class MultiWindow {
     public static void main(String[] args) {
